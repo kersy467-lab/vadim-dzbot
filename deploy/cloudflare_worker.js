fb4d3bce75b715d43f9a8eaf7efd463f9c66721a
k11298379-sudo <k11298379@gmail.com>
2026-09-24 19:01:41 +0500

    chore: update fallback render url to dzbot-hy44.onrender.com

--- deploy/cloudflare_worker.js
+++ deploy/cloudflare_worker.js
@@ -8,7 +8,7 @@
  */
 
 // Your Render backend URL (without trailing slash)
-const ORIGIN_URL = "https://dzbot-6eid.onrender.com";
+const ORIGIN_URL = "https://dzbot-hy44.onrender.com";
 
 // Cache TTL configurations (in seconds)
 const CACHE_RULES = [
